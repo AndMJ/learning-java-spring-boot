9b8efd3033f8cdf9b516c7cbc9c57187d046bcea
AndMJ <andrejoao070@gmail.com>
2023-01-20 20:47:48 +0000

    house cleaning

--- parking-control/src/main/java/com/api/parkingcontrol/DataTransferObjects/ParkingSpotDTO.java
+++ parking-control/src/main/java/com/api/parkingcontrol/DataTransferObjects/ParkingSpotDTO.java
@@ -21,4 +21,68 @@ public class ParkingSpotDTO {
     private String apartment;
     @NotBlank
     private String block;
+
+    public String getSpotNumber() {
+        return spotNumber;
+    }
+
+    public void setSpotNumber(String spotNumber) {
+        this.spotNumber = spotNumber;
+    }
+
+    public String getCarLicensePlate() {
+        return carLicensePlate;
+    }
+
+    public void setCarLicensePlate(String carLicensePlate) {
+        this.carLicensePlate = carLicensePlate;
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public String getCarColor() {
+        return carColor;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
 }
