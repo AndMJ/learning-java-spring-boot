0fe1f879fa588e6200b1826e472794d7bbd93c80
AndMJ <andrejoao070@gmail.com>
2023-01-20 20:03:46 +0000

    UPDATE save() method

--- parking-control/src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ parking-control/src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -1,20 +1,32 @@
 package com.api.parkingcontrol.controllers;
 
+import com.api.parkingcontrol.DataTransferObjects.ParkingSpotDTO;
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.services.ParkingSpotService;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/parking-spot")
+@RequestMapping("/api")
 public class ParkingSpotController {
 
     @Autowired
     public ParkingSpotService PSservice;
 
-    /*implement methods POST, GET, ... here*/
+    @PostMapping("/parking-spot")
+    public ResponseEntity<Object> save(@RequestBody @Valid ParkingSpotDTO PSDTO){
+        var PSmodel = new ParkingSpotModel(); //novo model de parking spot | novo registo
+        PSmodel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        BeanUtils.copyProperties(PSDTO, PSmodel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(PSservice.save(PSmodel));
+    }
 }
