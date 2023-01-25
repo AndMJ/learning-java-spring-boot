3ab0616d19c48b4649a18b3e6d331ecbdea06ca8
AndMJ <andrejoao070@gmail.com>
2023-01-25 03:04:51 +0000

    ADD getAll() & getByID()

--- parking-control/src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ parking-control/src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -5,6 +5,7 @@ import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.services.ParkingSpotService;
 
 import jakarta.validation.Valid;
+import org.json.simple.JSONObject;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,20 +14,42 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RequestMapping("/parking-spot")
+@RequestMapping("/api/parking-spot")
 public class ParkingSpotController {
 
-    @Autowired
+    @Autowired //SERVICE INJECTION
     public ParkingSpotService PSservice;
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<Object> save(@RequestBody @Valid ParkingSpotDTO PSDTO){
         var PSmodel = new ParkingSpotModel(); //novo model de parking spot | novo registo
         PSmodel.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
         BeanUtils.copyProperties(PSDTO, PSmodel);
         return ResponseEntity.status(HttpStatus.CREATED).body(PSservice.save(PSmodel));
+    }
+
+    @GetMapping("/list") //GET ALL
+    public ResponseEntity<List<ParkingSpotModel>> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(PSservice.getAll());
+    }
+
+    @GetMapping("/{id}") //GET BY ID
+    public ResponseEntity<Object> getById(@PathVariable(value = "id") UUID id){
+        Optional<ParkingSpotModel> dataFromDB = PSservice.getByID(id);
+
+        if (!dataFromDB.isPresent()) {
+            JSONObject msg = new JSONObject();
+            msg.put("message","Spot Not Found.");
+            msg.put("status", HttpStatus.NOT_FOUND.value());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(msg);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(dataFromDB);
     }
 }
--- parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -6,6 +6,10 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class ParkingSpotService {
 
@@ -15,5 +19,15 @@ public class ParkingSpotService {
     @Transactional
     public ParkingSpotModel save(ParkingSpotModel PSmodel) {
         return PSRepo.save(PSmodel); //add na DB e retorna mensagem de status
+    }
+
+    //GET ALL
+    public List<ParkingSpotModel> getAll(){
+        return PSRepo.findAll();
+    }
+
+    //GET BY ID
+    public Optional<ParkingSpotModel> getByID(UUID id){
+        return PSRepo.findById(id);
     }
 }
